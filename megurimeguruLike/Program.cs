15cb4baf74cf78ce6c585c5c47f4ca959862b53c
bacon <bacon0817.ix@gmail.com>
2023-07-06 17:28:41 +0900

    プロジェクト ファイルを追加します。

---
+++ megurimeguruLike/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MazeGame
+{
+    class Program
+    {
+
+        static char[][] map = new char[][]
+        {
+            new char[5]{ '#','#','#','#','#' },
+            new char[5]{ '#',' ',' ',' ','#' },
+            new char[5]{ '#',' ','#',' ','#' },
+            new char[5]{ '#','P','#','G','#' },
+            new char[5]{ '#','#','#','#','#' },
+
+        };
+
+        static void DrawMap()
+        {
+            for (int i = 0; i < map.Length; i++)
+            {
+                for (int j = 0; j < map[i].Length; j++)
+                {
+                    Console.Write(map[i][j]);
+                }
+                Console.Write(System.Environment.NewLine);
+            }
+        }
+
+        static Pxy GetP()
+        {
+            Pxy pxy = new Pxy();
+            for(int i = 0; i < map.Length;i++)
+            {
+                for(int j = 0; j< map[i].Length;j++)
+                {
+                    if (map[i][j] == 'P')
+                    {
+                        pxy.x = j;
+                        pxy.y = i;
+                    }
+                }
+            }
+            return pxy;
+        }
+
+        static void Main()
+        {
+            DrawMap();
+            string key = Console.ReadLine();
+            DrawMap();
+        }
+    }
+        class Pxy 
+        {
+            public int x;
+            public int y;
+        }
+}
